da9ff0280037f29bcd13cb095995501b7c2d4439
Alex Balis <AEBalis@yahoo.com>
2020-05-03 00:52:22 -0500

    init second hand completed

--- js.js
+++ js.js
@@ -1,7 +1,25 @@
 const secondHand = document.getElementById("second-hand");
+const minuteHand = document.getElementById("minute-hand");
+const hourHand = document.getElementById("hour-hand");
+
+let timestamp = new Date();
+let seconds = timestamp.getSeconds();
+let minutes = timestamp.getMinutes();
+let hours = timestamp.getHours();
+console.log(seconds);
+console.log(minutes);
+console.log(hours);
 
 
 
+if (seconds < 30) {
+  let rotation = (6 * (30 - seconds));
+  secondHand.style.transform = `rotateZ(-${rotation}deg)`;
+}
+if (seconds > 30) {
+  let rotation = (6 * (seconds - 30));
+  secondHand.style.transform = `rotateZ(${rotation}deg)`;
+}
 
 const secondTick = function () {
   let counter = 6
@@ -13,7 +31,30 @@ const secondTick = function () {
   }, 1000);
 }
 
-secondTick();
+const minuteTick = function () {
+  let counter = 0.1
+  setInterval(function () {
+
+    minuteHand.style.transform = `rotateZ(${counter}deg)`;
+    counter += 0.1;
+
+  }, 1000);
+}
+
+const hourTick = function () {
+  //inaccurate...0.00833 repeating...how to account for this?
+  let counter = 0.00833
+  setInterval(function () {
+
+    minuteHand.style.transform = `rotateZ(${counter}deg)`;
+    counter += 0.00833;
+
+  }, 1000);
+}
+
+//secondTick();
+minuteTick();
+hourTick();
 
 
 
